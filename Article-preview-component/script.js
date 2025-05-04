d43efe3ce971238c84019f130555faca423dcf99
Kestutis Eirosius <eirosiuss@gmail.com>
2025-05-04 18:40:15 +0300

    Implement share links functionality with responsive design adjustments

--- Article-preview-component/script.js
+++ Article-preview-component/script.js
@@ -1,4 +1,19 @@
 let button = document.querySelector('#button')
-console.log(button);
+
+let shareLinks = document.querySelector('#share-links')
+
+button.addEventListener('click', () => {
+    shareLinks.style.display = 'flex'
+})
+
+let backdrop = document.querySelector('#backdrop')
 
 
+backdrop.addEventListener('click', () => {
+
+    shareLinks.style.display = 'none'
+})
+
+console.log(button); // Turi rodyti elementą, ne null
+console.log(shareLinks); // Turi rodyti elementą, ne null
+console.log(backdrop); // Turi rodyti elementą, ne null
